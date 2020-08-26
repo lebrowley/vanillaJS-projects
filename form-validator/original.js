23c26b7c91a700c3c3be005aacc9ca3c570cad31
laurenebr <lauren@ozmktg.com>
2020-08-26 15:30:32 -0600

    first commit

---
+++ form-validator/original.js
@@ -0,0 +1,59 @@
+//pull in all the DOM elements we need
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+//Functions
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isValidEmail(email) {
+  //js email regex from stackoverflow
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+//Event Listeners
+//add an event listener on the form for when we submit it
+form.addEventListener("submit", function (e) {
+  //by preventing the defaul behavior of the event object (e), we can intercept     the information from the form and validate it
+  e.preventDefault();
+
+  //if you just do console.log(username), you'll get the actual HTML element, but   we want to be able to access what was type in the input box, or the value.
+  //so access it off the element (which was grabbed by it's id and set to the   variable username above)
+  //console.log(username.value);
+
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "Password is required");
+  } else {
+    showSuccess(password2);
+  }
+});
